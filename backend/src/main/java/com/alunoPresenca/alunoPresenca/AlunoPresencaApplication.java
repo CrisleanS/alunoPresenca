6f223433b313980e626701c75bd5b1280109b6d4
Gustavo Aleixo <gustavoaleixo2000@gmail.com>
2024-04-01 13:31:14 -0300

    initial commit

---
+++ backend/src/main/java/com/alunoPresenca/alunoPresenca/AlunoPresencaApplication.java
@@ -0,0 +1,13 @@
+package com.alunoPresenca.alunoPresenca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoPresencaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoPresencaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alunoPresenca/alunoPresenca/AlunoPresencaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alunoPresenca.alunoPresenca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunoPresencaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
